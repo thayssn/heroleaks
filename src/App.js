8afecd9b847f5b3e706ae9983badbf8d226c3e6d
Thays dos Santos <thays.santos@omeletecompany.com>
2021-06-14 07:39:19 -0300

    Projeto heroleaks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import { debounce, findInString, logSkull } from './utils'
+
+const Header = () => <header>
+  <pre>
+  <span>||█ TOP SECRET █|███</span>
+  <em></em>
+  </pre>
+  </header>
+const Footer = () => <footer>
+  <div>
+  * -----------------------------------------------------------------------------  *
+    <div>
+      * heroleaks 💀 property of  <span>Thays</span> <span>dos</span> <span>Santos</span> <span>Neves</span>
+      , published at <a href="https://github.com/thayssn">github@thayssn</a>
     </div>
+  * -----------------------------------------------------------------------------  *
+  </div>
+  </footer>
+const Loading = () => <><span className=" glass">🔎</span><span>searching database</span></>
+
+async function fetchIdentities(){
+  const response = await fetch('/identities.json')
+  return await response.json()
+
+}
+
+const App = () => {
+  const [ identities, setIdentities ] = useState([])
+  const [ sugestions, setSugestions ] = useState([])
+  const [ knownWords, setKnownWords ] = useState([])
+  const [ loading, setLoading ] = useState(false)
+  const [ error, setError ] = useState('')
+  const inputRef = document.querySelector('form input');
+
+  async function fetchNames(){
+    const response = await fetch('/heroes.json')
+    const data = await response.json()
+    setKnownWords(data);
+  }
+
+  useEffect(() => {
+    logSkull()
+    fetchNames()
+
+  },[])
+
+  const getSugestions = debounce(async (term) => {
+    if(!term) {
+      setSugestions([])
+      return;
+    };
+
+    const possibleWords = knownWords.filter(word => findInString(word,term))
+    setSugestions(possibleWords);
+  }, 500)
+
+  function handleInputChange (e) {
+    getSugestions(e.target.value)
+  }
+
+  function selectSugestion(e) {
+    setSugestions([])
+    inputRef.value = e.target.textContent
+    inputRef.focus()
+  }
+  async function handleFormSubmit(e) {
+    e.preventDefault()
+    const term = inputRef.value;
+
+    if(!term) return;
+
+    setLoading(true)
+
+    setTimeout(async () => {
+      const fetchedIdentities = await fetchIdentities()
+      const filteredIdentities = fetchedIdentities.filter(hero => 
+        findInString(hero.name, term)
+      )
+  
+      setIdentities(filteredIdentities)
+      setLoading(false)
+      setSugestions([])
+  
+      if(!filteredIdentities.length) setError(`No hero named '${inputRef.value}'`)
+    },500)
+  }
+
+  window.addEventListener('keydown', (event) => {
+    if(event.code === 'Escape'){
+      setSugestions([])
+    }
+  });
+  
+  return (
+    <>
+    <Header />
+    <section className="App">
+      <h1>Use the box to search for the secret identities</h1>
+      <form onSubmit={handleFormSubmit}>
+        <div className="inputGroup">
+        <input type="text" placeholder="Search for identity" onChange={handleInputChange} autoFocus onFocusOut={() => { setSugestions([])}}/>
+          <ul className="sugestions">
+            {sugestions.map((word, idx) => <button type="button" key={idx} onClick={selectSugestion}>{word}</button>)}
+          </ul>
+        </div>
+        <button type="submit" disabled={loading}>{ loading ? <Loading /> : 'search'}</button>
+      </form>
+      {identities.length ? <>
+        <ul className="identities">
+          {identities.map((hero, idx) => <li key={idx}><button title={hero.name}>{hero.name}</button> - <button className="stripe" title={hero.identity}>{hero.identity}</button></li>)}
+        </ul>
+      </>
+      : <p className="error">{error}</p>
+      }
+    </section>
+    <Footer />
+    </>
   );
 }
 
---
+++ src/utils.js
@@ -0,0 +1,52 @@
+let debounceTimer;
+export function debounce(fn, delay){
+  return function (...args) {
+    const callLaterFn = () => {
+      fn(...args)
+    }
+
+    clearTimeout(debounceTimer)
+    debounceTimer = setTimeout(callLaterFn, delay);
+  }
+}
+
+export function findInString(source, string){
+  return source.toLowerCase().includes(string.toLowerCase())
+}
+
+export function logSkull(){
+console.log(`%c
+                     .ed"""" """$$$$be.
+                   -"           ^""**$$$e.
+                 ."                   '$$$c
+                /                      "4$$b
+               d  3                      $$$$
+               $  *                   .$$$$$$
+              .$  ^c           $$$$$e$$$$$$$$.
+              d$L  4.         4$$$$$$$$$$$$$$b
+              $$$$b ^ceeeee.  4$$ECL.F*$$$$$$$
+  e$""=.      $$$$P d$$$$F $ $$$$$$$$$- $$$$$$
+ z$$b. ^c     3$$$F "$$$$b   $"$$$$$$$  $$$$*"      .=""$c
+4$$$$L        $$P"  "$$b   .$ $$$$$...e$$        .=  e$$$.
+^*$$$$$c  %..   *c    ..    $$ 3$$$$$$$$$$eF     zP  d$$$$$
+  "**$$$ec   "   %ce""    $$$  $$$$$$$$$$*    .r" =$$$$P""
+        "*$b.  "c  *$e.    *** d$$$$$"L$$    .d"  e$$***"
+          ^*$$c ^$c $$$      4J$$$$$% $$$ .e*".eeP"
+             "$$$$$$"'$=e....$*$$**$cz$$" "..d$*"
+               "*$$$  *=%4.$ L L$ P3$$$F $$$P"
+                  "$   "%*ebJLzb$e$$$$$b $P"
+                    %..      4$$$$$$$$$$ "
+                     $$$e   z$$$$$$$$$$%
+                      "*$c  "$$$$$$$P"
+                       ."""*$$$$$$$$bc
+                    .-"    .$***$$$"""*e.
+                 .-"    .e$"     "*$c  ^*b.
+          .=*""""    .e$*"          "*bc  "*$e..
+        .$"        .z*"               ^*$e.   "*****e.
+        $$ee$c   .d"                     "*$.        3.
+        ^*$E")$..$"                         *   .ee==d%
+           $.d$$$*                           *  J$$$e*
+            """""                              "$$$"
+
+`, "color: crimson;")
+}
